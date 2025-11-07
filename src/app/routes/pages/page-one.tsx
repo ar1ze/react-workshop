c589222698259e9a0e5d744e381998d344081a48
Eric Le <eriiicle@outlook.com>
2025-11-07 23:04:39 +0100

    feat(pages): add page one and page two components

---
+++ src/app/routes/pages/page-one.tsx
@@ -0,0 +1,12 @@
+const PageOne = () => {
+  return (
+    <div className="grid grid-rows-[auto_1fr] gap-y-2">
+      <h1 className="text-xl font-medium">Page One</h1>
+      <div className="text-sm">
+        <p>This is the content for Page One.</p>
+      </div>
+    </div>
+  )
+}
+
+export default PageOne
---
+++ src/app/routes/pages/page-two.tsx
@@ -0,0 +1,12 @@
+const PageTwo = () => {
+  return (
+    <div className="grid grid-rows-[auto_1fr] gap-y-2">
+      <h1 className="text-xl font-medium">Page Two</h1>
+      <div className="text-sm">
+        <p>This is the content for Page Two.</p>
+      </div>
+    </div>
+  )
+}
+
+export default PageTwo
