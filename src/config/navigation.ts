e66391a38ac3eb7cc6d2404da9fd2053f3740a06
Eric Le <eriiicle@outlook.com>
2025-11-08 17:55:16 +0100

    feat(navigation): add navigation sections to config

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -7,3 +7,28 @@ export const navigationLinks: NavigationLink[] = [
   { to: '/page-one', label: 'Page One' },
   { to: '/page-two', label: 'Page Two' },
 ]
+
+export interface NavigationSection {
+  id: string
+  label: string
+  links: NavigationLink[]
+}
+
+export const navigationSections: NavigationSection[] = [
+  {
+    id: 'page-section-one',
+    label: 'Page Section One',
+    links: [
+      { to: 'page-section-one/subpage-one', label: 'Sub Page One ' },
+      { to: 'page-section-one/subpage-two', label: 'Sub Page Two ' },
+    ],
+  },
+  {
+    id: 'page-section-two',
+    label: 'Page Section Two',
+    links: [
+      { to: 'page-section-two/subpage-one', label: 'Sub Page One ' },
+      { to: 'page-section-two/subpage-two', label: 'Sub Page Two ' },
+    ],
+  },
+]
