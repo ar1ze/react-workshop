8363a7c3b0cd4150224f909fa3fed1e8d1eb2f39
Eric Le <eriiicle@outlook.com>
2025-11-13 21:28:24 +0100

    refactor(layout): make title prop optional

--- src/components/layout/content-layout.tsx
+++ src/components/layout/content-layout.tsx
@@ -1,15 +1,15 @@
 import { type ReactNode } from 'react'
 
 interface PageLayoutProps {
-  title: string
+  title?: string
   children: ReactNode
 }
 
 export const ContentLayout = ({ title, children }: PageLayoutProps) => {
   return (
     <div className="grid grid-rows-[auto_1fr] gap-y-2 px-4">
-      <h1 className="text-xl font-medium">{title}</h1>
-      <div className="text-sm">{children}</div>
+      {title && <h1 className="text-xl font-medium">{title}</h1>}
+      {children}
     </div>
   )
 }
