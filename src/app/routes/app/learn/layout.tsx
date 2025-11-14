d02e63f1ca6541230222da5b773e2072da63a6d7
Eric Le <eriiicle@outlook.com>
2025-11-14 01:58:47 +0100

    feat(learn): add sidebar layout for learn page

--- src/app/routes/app/learn/layout.tsx
+++ src/app/routes/app/learn/layout.tsx
@@ -1,5 +1,21 @@
 import { Outlet } from 'react-router'
 
+import { SidebarLayout } from '@/components/layouts'
+import { Sidebar } from '@/components/ui/sidebar'
+
+const createSidebar = () => {
+  return (
+    <Sidebar className="border-r px-4">
+      <p>Sidebar</p>
+    </Sidebar>
+  )
+}
+
 export const LearnPageLayout = () => {
-  return <Outlet />
+  const sidebar = createSidebar()
+  return (
+    <SidebarLayout sidebar={sidebar}>
+      <Outlet />
+    </SidebarLayout>
+  )
 }
