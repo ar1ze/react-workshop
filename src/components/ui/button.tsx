c79e5a72293b66d80159847ed85cc2050efba2c7
Eric Le <eriiicle@outlook.com>
2025-11-18 14:17:43 +0100

    style(ui): manually update accordion, button, and input components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,28 +1,37 @@
 import { Slot } from '@radix-ui/react-slot'
 import { type VariantProps } from 'class-variance-authority'
 import { type ButtonHTMLAttributes } from 'react'
-import { forwardRef } from 'react'
+import { type ComponentProps } from 'react'
 
 import { cn } from '@/lib/utils'
 
 import { buttonVariants } from './button-variants'
 
-export interface ButtonProps
+interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button'
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = 'Button'
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot : 'button'
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
+
+export { Button, type ButtonProps }
