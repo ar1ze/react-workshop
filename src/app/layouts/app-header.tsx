88247700005d079f217f5e744b78d0e206176652
Eric Le <eriiicle@outlook.com>
2025-11-19 06:34:28 +0100

    refactor(layout): restructure app header and simplify action button props

--- src/app/layouts/app-header.tsx
+++ src/app/layouts/app-header.tsx
@@ -1,21 +1,18 @@
-import { type LucideProps } from 'lucide-react'
 import { NavLink } from 'react-router'
 
 import { IconButton } from '@/components/common'
 import { GithubIcon } from '@/components/icons'
+import { NavigationLinkStyled } from '@/components/navigation'
 import { ThemeButton } from '@/components/theme'
-import { type ButtonProps } from '@/components/ui/button'
 
 import { BLOG_PAGE_PREFIX } from '../routes/app/blog'
 import { LEARN_PAGE_PREFIX } from '../routes/app/learn'
 
-const BrandLink = () => {
-  return (
-    <NavLink to="/" className="text-muted-background text-xl font-bold">
-      React Workshop
-    </NavLink>
-  )
-}
+const BrandLink = () => (
+  <NavLink to="/" className="text-muted-background text-xl font-bold">
+    <span>React Workshop</span>
+  </NavLink>
+)
 
 const HeaderNavLinks = () => {
   const navLinks = [
@@ -26,57 +23,37 @@ const HeaderNavLinks = () => {
   return (
     <nav className="flex gap-4">
       {navLinks.map(({ to, label }) => (
-        <NavLink
+        <NavigationLinkStyled
           key={to}
           to={to}
-          className={({ isActive }) =>
-            `hover:text-primary text-base font-medium transition-colors ${
-              isActive ? '' : 'text-muted-foreground'
-            }`
-          }
-        >
-          {label}
-        </NavLink>
+          label={label}
+          className="text-base font-medium"
+        />
       ))}
     </nav>
   )
 }
 
-const HeaderActions = () => {
-  const buttonProps: ButtonProps = {
-    className: '[&_svg]:size-6',
-  }
-  const iconProps: LucideProps = {
-    size: 48,
-  }
-  const githubIcon = <GithubIcon {...iconProps} />
+const HeaderActions = () => (
+  <div className="flex">
+    <IconButton
+      icon={<GithubIcon />}
+      href="https://github.com/ar1ze/react-workshop"
+    />
+    <ThemeButton />
+  </div>
+)
 
-  return (
-    <div className="flex gap-2">
-      <ThemeButton iconProps={iconProps} buttonProps={buttonProps} />
-      <IconButton
-        icon={githubIcon}
-        href="https://github.com/ar1ze/react-workshop"
-        {...buttonProps}
-      />
-    </div>
-  )
-}
+const HeaderNavigation = () => (
+  <div className="flex items-center gap-2 md:gap-4">
+    <HeaderNavLinks />
+    <HeaderActions />
+  </div>
+)
 
-const HeaderNavigation = () => {
-  return (
-    <div className="flex items-center gap-6">
-      <HeaderNavLinks />
-      <HeaderActions />
-    </div>
-  )
-}
-
-export const AppHeader = () => {
-  return (
-    <header className="flex justify-between p-4">
-      <BrandLink />
-      <HeaderNavigation />
-    </header>
-  )
-}
+export const AppHeader = () => (
+  <header className="flex items-center justify-between p-4">
+    <BrandLink />
+    <HeaderNavigation />
+  </header>
+)
