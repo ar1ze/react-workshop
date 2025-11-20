1b5b47f9f7cb7ce598391af3a9cec17bfea01b9c
Eric Le <eriiicle@outlook.com>
2025-11-20 14:44:14 +0100

    feat(navigation): add onclick prop to navigation link styled

--- src/components/navigation/link.tsx
+++ src/components/navigation/link.tsx
@@ -1,15 +1,19 @@
+import type { MouseEventHandler } from 'react'
 import { NavLink } from 'react-router'
 import { twMerge } from 'tailwind-merge'
 
 import { type NavigationLink } from '@/types/navigation'
 import { type BaseProps } from '@/types/props'
 
-interface NavigationLinkStyledProps extends NavigationLink, BaseProps {}
+interface NavigationLinkStyledProps extends NavigationLink, BaseProps {
+  onClick?: MouseEventHandler<HTMLAnchorElement>
+}
 
 export const NavigationLinkStyled = ({
   to,
   label,
   className,
+  ...props
 }: NavigationLinkStyledProps) => {
   return (
     <NavLink
@@ -22,6 +26,7 @@ export const NavigationLinkStyled = ({
           className
         )
       }
+      {...props}
     >
       {label}
     </NavLink>
