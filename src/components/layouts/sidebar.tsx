44dd624fb52592808e14aee1b128d872a1ca5960
Eric Le <eriiicle@outlook.com>
2025-11-21 09:00:45 +0100

    feat(layout): add style prop to sidebar layout component

--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -1,19 +1,21 @@
-import { type ReactNode } from 'react'
+import { type CSSProperties, type ReactNode } from 'react'
 
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { type BaseProps } from '@/types/props'
 
 export interface SidebarLayoutProps extends BaseProps {
   sidebar: ReactNode
+  style?: CSSProperties
 }
 
 export const SidebarLayout = ({
   sidebar,
   children,
   className,
+  style,
 }: SidebarLayoutProps) => {
   return (
-    <SidebarProvider className={className}>
+    <SidebarProvider className={className} style={style}>
       {sidebar}
       {children}
     </SidebarProvider>
