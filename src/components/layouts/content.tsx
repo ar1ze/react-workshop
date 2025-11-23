18c0ade15d7a255ff5de06ccf940ac27fd202967
Eric Le <eriiicle@outlook.com>
2025-11-23 05:17:03 +0100

    fix(components): content layout props import and export

--- src/components/layouts/content.tsx
+++ src/components/layouts/content.tsx
@@ -1,34 +1,5 @@
-import { cn } from '@/lib/utils'
 import { type BaseProps } from '@/types/props'
 
-export interface LayoutTitleProps {
-  title?: string
-  titleClassName?: string
-}
-
-export interface ContentLayoutProps extends LayoutTitleProps, BaseProps {}
-
-const LayoutTitle = ({ title, titleClassName }: LayoutTitleProps) => {
-  const defaultTitleClasses = 'text-xl font-medium'
-
-  if (!title) return null
-  return <h1 className={cn(defaultTitleClasses, titleClassName)}>{title}</h1>
-}
-
-export const ContentLayout = ({
-  title,
-  titleClassName,
-  className,
-  children,
-}: ContentLayoutProps) => {
-  const defaultMainClasses = title
-    ? 'grid grid-rows-[auto_1fr] gap-y-2 px-4'
-    : 'px-4'
-
-  return (
-    <main className={cn(defaultMainClasses, className)}>
-      <LayoutTitle title={title} titleClassName={titleClassName} />
-      {children}
-    </main>
-  )
+export const ContentLayout = ({ className, children }: BaseProps) => {
+  return <main className={className}>{children}</main>
 }
--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,10 +1,9 @@
-import { ContentLayout, type ContentLayoutProps } from './content'
+import { ContentLayout } from './content'
 import { HeaderLayout, type HeaderLayoutProps } from './header'
 import { SidebarLayout, type SidebarLayoutProps } from './sidebar'
 
 export {
   ContentLayout,
-  type ContentLayoutProps,
   HeaderLayout,
   type HeaderLayoutProps,
   SidebarLayout,
