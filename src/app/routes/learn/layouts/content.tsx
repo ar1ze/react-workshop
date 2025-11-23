66908076c350faf46622017ee303f1d6b72480f2
Eric Le <eriiicle@outlook.com>
2025-11-23 07:09:11 +0100

    refactor(learn): simplify content layout responsiveness

--- src/app/routes/learn/layouts/content.tsx
+++ src/app/routes/learn/layouts/content.tsx
@@ -13,7 +13,7 @@ export const LearnContentLayout = ({
 }: LearnContentLayoutProps) => {
   return (
     <AppContentLayout className={cn('flex', className)}>
-      <section className="w-full md:mx-auto md:max-w-120 lg:max-w-180 xl:max-w-260">
+      <section className="w-full md:mx-8 lg:mx-auto lg:max-w-200 xl:max-w-260">
         {title && <SectionTitle>{title}</SectionTitle>}
         {children}
       </section>
