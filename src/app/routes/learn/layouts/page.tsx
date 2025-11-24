53ef991ffb866266a60d6ea20db3fea6e8be4995
Eric Le <eriiicle@outlook.com>
2025-11-24 06:00:20 +0100

    style(learn): increase gap in page layout

--- src/app/routes/learn/layouts/page.tsx
+++ src/app/routes/learn/layouts/page.tsx
@@ -2,5 +2,5 @@ import { cn } from '@/lib/utils'
 import { type BaseProps } from '@/types/props'
 
 export const LearnPageLayout = ({ className, children }: BaseProps) => {
-  return <div className={cn('flex flex-col gap-6', className)}>{children}</div>
+  return <div className={cn('flex flex-col gap-8', className)}>{children}</div>
 }
