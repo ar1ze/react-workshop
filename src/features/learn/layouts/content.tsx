981b9b3e9300c5151da263688e30bf14e4766e6b
Eric Le <eriiicle@outlook.com>
2025-11-26 18:23:15 +0100

    style(learn): add bottom padding to content layout

--- src/features/learn/layouts/content.tsx
+++ src/features/learn/layouts/content.tsx
@@ -14,7 +14,7 @@ export const LearnContentLayout = ({
 }: LearnContentLayoutProps) => {
   return (
     <AppContentLayout className={cn('flex', className)}>
-      <section className="w-full md:mx-8 lg:mx-auto lg:max-w-200 xl:max-w-260">
+      <section className="w-full pb-20 md:mx-8 lg:mx-auto lg:max-w-200 xl:max-w-260">
         {title && <TypographyH1>{title}</TypographyH1>}
         {children}
       </section>
