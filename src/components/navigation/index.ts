0b69358fb33144fb79724a9939d04d4ebae39c44
Eric Le <eriiicle@outlook.com>
2025-11-29 17:29:16 +0100

    refactor(navigation): simplify module exports

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,16 +1,4 @@
-import {
-  NavigationAccordionLinks,
-  NavigationAccordionTrigger,
-} from './accordion'
-import { NavigationButton } from './button'
-import { NavigationLinkStyled } from './link'
-import type { NavigationLink, NavigationNode } from './types'
-
-export {
-  NavigationAccordionLinks,
-  NavigationAccordionTrigger,
-  NavigationButton,
-  type NavigationLink,
-  NavigationLinkStyled,
-  type NavigationNode,
-}
+export * from './accordion'
+export * from './button'
+export * from './link'
+export * from './types'
