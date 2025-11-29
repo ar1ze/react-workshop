8e4e54d7875dd148ae33e8b8c79b07458ee40a3e
Eric Le <eriiicle@outlook.com>
2025-11-29 21:12:52 +0100

    feat(learn): integrate previous/next navigation into page layout

--- src/features/learn/layouts/page.tsx
+++ src/features/learn/layouts/page.tsx
@@ -1,8 +1,12 @@
 import { type BaseProps } from '@/components/shared/props'
+import { LearnNavigationPreviousNext } from '@/features/learn/components'
 import { cn } from '@/lib/utils'
 
 export const LearnPageLayout = ({ className, children }: BaseProps) => {
   return (
-    <div className={cn('flex flex-col gap-6 pb-12', className)}>{children}</div>
+    <div className={cn('flex flex-col gap-6 pb-8', className)}>
+      {children}
+      <LearnNavigationPreviousNext />
+    </div>
   )
 }
